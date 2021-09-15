68d6c49320d1ff4d59fb19c16163638c0df39bfa
Breno Moura <89282568+Breno-Moura@users.noreply.github.com>
2021-09-14 21:32:17 -0300

    Delete ALGORITMOS directory

--- ALGORITMOS/aula1.py
+++
@@ -1,9 +0,0 @@
-content = input("Type a simple world or phrase: ")
-content_cyphered = ""
-for letter in content:
-    if letter in "aeiou":
-        content_cyphered += '*'
-    else:
-        content_cyphered += letter
-
-print("Cyphered content - %s" %content_cyphered)
--- ALGORITMOS/exercicio1.py
+++
@@ -1,9 +0,0 @@
-import sys
-a = input("Digite um número de 0 a 10:\n")
-a = int(a)
-while a > 10 or a < 0:
-    print ("Número inválido!")
-    a = input("Digite um número de 0 a 10:\n")
-    a = int(a)
-print("Número válido!")
-sys.exit()
