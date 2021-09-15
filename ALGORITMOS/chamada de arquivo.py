d3e464aaeb282c30dbfa974815103e4ff36dc944
Breno Moura <89282568+Breno-Moura@users.noreply.github.com>
2021-09-14 21:34:34 -0300

    Add files via upload

---
+++ ALGORITMOS/chamada de arquivo.py
@@ -0,0 +1,8 @@
+import busca_binaria
+
+lista_simples = ["f", "b", "c", "j", "a", "e"]
+j = 1
+
+print(busca_binaria.busca_binaria(sorted(lista_simples), "j"))
+
+print(busca_binaria.busca_simples(lista_simples, "j"))
